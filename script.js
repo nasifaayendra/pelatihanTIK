63b7fa8db5e513caf1401876fea0c4158702ccd5
nasifaayendra <nasifaayendra@gmail.com>
2025-10-02 12:12:23 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+const tombolSapa = document.getElementById('sapaButton')
+tombolSapa.addEventListener('click',function(){
+  alert('Halo!Terima kasih sudah berkunjung!');
+});
+
+                            
